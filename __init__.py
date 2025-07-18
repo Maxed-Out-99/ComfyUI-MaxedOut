8aef0a1fd11cba6419a7fad6bffd1b550674150f
Maxed-Out-99 <maxedout99999@gmail.com>
2025-07-18 14:02:50 -0700

    Add web directory constant (#6)

--- __init__.py
+++ __init__.py
@@ -3,7 +3,10 @@ from .maxedoutnodes import (
     NODE_DISPLAY_NAME_MAPPINGS,
 )
 
+WEB_DIRECTORY = "web"
+
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
+    "WEB_DIRECTORY",
 ]
